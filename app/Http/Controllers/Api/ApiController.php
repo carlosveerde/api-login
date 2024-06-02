92bb3f81794ea110b8b7d7aefeacc8d4306ba03f
Verde <carlos-verde@outlook.com>
2024-06-02 15:30:17 -0300

    feat: :sparkles: Add register API endpoint and user registration functionality

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -4,12 +4,42 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class ApiController extends Controller
 {
     public function register(Request $request)
     {
-        echo "Register Api";
+        $validateUser = Validator::make($request->all(),
+        [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        if($validateUser->fails()){
+            return response()->json([
+                'status' => false,
+                'message' => 'validation error',
+                'errors' => $validateUser->errors()
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User created succesfully',
+            'token' => $user->createToken("API TOKEN")->plainTextToken
+        ], 200);
+
     }
 }
 
